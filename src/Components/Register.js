f3c774982101000a91d0f731a0b0282907bfddfb
Ariful Islam <arifulislam161024@gmail.com>
2021-11-23 18:26:15 +0600

    Update the email regax

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -30,7 +30,7 @@ const Register = () => {
 		}
 	};
 	// email validation
-	const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 	const validateEmail = (event) => {
 		const email = event.target.value;
 		if (emailRegex.test(email)) {
